d66be043b7cb8b0f96ec1fa28d7ed1b8c0dd2972
Adam <130487352+linuxcomix@users.noreply.github.com>
2023-05-23 16:02:30 +0100

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner names = new Scanner(System.in);
+
+        System.out.println("enter your name: ");
+        String name = names.nextLine();
+
+        System.out.println("Hello, "+name);
+    }
+}
